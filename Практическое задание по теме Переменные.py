39113ff3cea7eba49fc6ba13382fcdaa70b3b15b
Goomb007 <goomboo@yandex.ru>
2024-08-12 11:07:30 +0300

    Add files via upload

---
+++ Практическое задание по теме Переменные.py
@@ -0,0 +1,5 @@
+summ_of_homework = 12
+time_spend = 1.5
+course_name = 'Python'
+one_task_time = time_spend / summ_of_homework
+print("Курс: ", course_name + ",", "всего задач:" + str(summ_of_homework) + ",", "затраченно часов:", str(time_spend) + ",", "среднее время выполнения",one_task_time,"часа.")
